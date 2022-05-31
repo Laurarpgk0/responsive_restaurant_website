bbb0fca9bae235bf4cdde4668dbf41f7fcfd7a74
Laurarpgk0 <laurazt6@gmail.com>
2022-05-31 19:34:07 -0300

    feat: First Commit

---
+++ src/container/Chef/Chef.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
+import "./Chef.css";
+
+const Chef = () => (
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          </p>
+        </div>
+        <p className="p__opensans">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Necessitatibus rerum sint est, soluta eos laudantium. Tempore ducimus
+          sit ab velit, voluptatem repudiandae laudantium doloribus, tempora et
+          fugit illum nulla ad!
+        </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Chef;
